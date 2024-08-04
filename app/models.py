6760cb9aa71307d4753477447c0fe864281b0e58
fernando-izar <fizar@me.com>
2024-08-04 15:39:04 -0300

    feat: reviews, create review, load_reviews_from_excel, scrape_reviews

--- app/models.py
+++ app/models.py
@@ -1,14 +1,35 @@
 from pydantic import BaseModel, Field, field_validator
 from typing import Optional
 from datetime import date
-from decimal import Decimal
+from decimal import Decimal, ROUND_HALF_UP
+import re
 
 class RestaurantReview(BaseModel):
     data: date = Field(..., title="Review Date")
     reviewer: str = Field(..., title="Reviewer Name", min_length=1, max_length=100)
-    testo: Optional[str] = Field(None, title="Review Text", max_length=1000)
+    testo: Optional[str] = Field(None, title="Review Text", max_length=500)
     sentiment: Optional[int] = Field(None, title="Sentiment")
     voto: Decimal = Field(..., title="Rating", ge=0, le=5)
+
+    @field_validator('data')
+    def validate_data(cls, data):
+        if data > date.today():
+            raise ValueError("Review date can't be in the future.")
+        return data
+
+
+    @field_validator('reviewer')
+    def validate_reviewer(cls, reviewer):
+        reviewer = re.sub(r'[^\w\s]', '', reviewer)
+        if len(reviewer) > 100:
+            reviewer = reviewer[:100]
+        return reviewer
+
+    @field_validator('testo')
+    def validate_testo(cls, testo):
+        if testo and len(testo) > 500:
+            testo = testo[:500]
+        return testo
 
     @field_validator('sentiment')
     def validate_sentiment(cls, sentiment):
@@ -18,8 +39,10 @@ class RestaurantReview(BaseModel):
     
     @field_validator('voto')
     def validate_voto(cls, voto):
-        if voto < 0 or voto > 5:
-            raise ValueError("Rating must be between 0 an 5")
-        if voto.as_tuple().exponent < -1:
-            raise ValueError("Rating must have at most one decimal place")
+        if voto < 0:
+            voto = Decimal(0)
+        elif voto > 5:
+            voto = Decimal(5)
+        voto = voto.quantize(Decimal('0.0'), rounding=ROUND_HALF_UP)
+        return voto
     
---
+++ app/services/scraper.py
@@ -0,0 +1,98 @@
+import requests
+from bs4 import BeautifulSoup
+from typing import List
+from datetime import datetime
+from app.models import RestaurantReview
+from decimal import Decimal
+import ipdb
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import locale
+
+locale.setlocale(locale.LC_TIME, 'it_IT.UTF-8')
+
+def scrape_reviews(restaurant_name: str) -> List[RestaurantReview]:
+    url = f"https://www.justeat.it/restaurants-{restaurant_name}/reviews"
+
+    # ChromeDriver Configuration
+    # service = Service(ChromeDriverManager().install())
+    options = webdriver.ChromeOptions()
+    options.add_argument('--headless')
+    options.add_argument('--no-sandbox')
+    options.add_argument('--disable-dev-shm-usage')
+    driver = webdriver.Chrome()
+
+    try:
+        driver.get(url)
+        # element = WebDriverWait(driver, 10).until(
+        #     EC.presence_of_element_located([(By.CLASS_NAME, "c-reviews-items"), (By.CLASS_NAME, "vEI0Do")])
+        # )
+        # if not element:
+        #     return {"message": "No reviews found"}
+        page_source = driver.page_source
+    finally:
+        ...
+        driver.quit()
+
+    # Gravar o conteúdo de page_source em um arquivo externo
+    with open('page_source.html', 'w', encoding='utf-8') as file:
+        file.write(page_source)
+
+
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'
+    }
+    
+
+    # try:
+    #     response = requests.get(url, headers=headers)
+    #     response.raise_for_status()  
+    # except requests.exceptions.RequestException as e:
+    #     raise Exception(f"Failed to fetch reviews for {restaurant_name}: {e}")
+    
+
+    soup = BeautifulSoup(page_source, "html.parser")
+    reviews = []
+    review_elements = soup.find_all('div', class_='vEI0Do')
+    print(len(review_elements))
+    if not review_elements:
+        review_elememnts = soup.find_all('div', class_='c-reviews-item') 
+    
+    for element in review_elements:
+        # Extract reviewer
+        reviewer_element = element.find('div', class_='_1AzTo')
+        reviewer = reviewer_element.text.strip() if reviewer_element else 'Test User'
+        
+        # Extract date
+        date_str_element = element.find('b', class_='Tcels') or element.find('b', class_='c-reviews-item-date')
+        date_str = date_str_element.text.strip() if date_str_element else None
+        review_date = datetime.strptime(date_str, '%A %d %B %Y').date() if date_str else datetime.now().date()
+        
+        # Extract rating
+        # rating_div = element.find('div', class_='_15tmo', attrs={'data-qa': 'rating-display-element'})
+        # voto_title = rating_div['title']
+        # voto = Decimal(voto_title.split(' ')[0])  
+        voto = 5
+        # Extract text
+        testo_element = element.find('b', class_='_3Clmt')
+        if not testo_element:
+            testo_element = element.find('b', attrs={'data-test-id': 'review-text'})
+        testo = testo_element.text if testo_element else None
+        
+        # Sentiment is set to None as per requirement
+        sentiment = None
+        
+        review = RestaurantReview(
+            data=review_date,
+            reviewer=reviewer,
+            testo=testo,
+            sentiment=sentiment,
+            voto=voto
+        )
+        reviews.append(review)
+
+    return reviews
